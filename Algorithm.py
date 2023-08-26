986a24f0ac93fe84fa0c398162265bdf90cfef78
soundwave <45279161+CleberYoo@users.noreply.github.com>
2023-08-26 18:50:34 +0900

    Update Algorithm.py

--- Algorithm.py
+++ Algorithm.py
@@ -513,3 +513,37 @@ def q1110():
             break
 
     print(counter)
+    
+def q2577():
+    A = int(input())
+    B = int(input())
+    C = int(input())
+
+    D = str(A * B * C)
+
+    counter = {'0' : 0,
+            '1' : 0,
+            '2' : 0,
+            '3' : 0,
+            '4' : 0,
+            '5' : 0,
+            '6' : 0,
+            '7' : 0,
+            '8' : 0,
+            '9' : 0}
+
+    for d in D:
+        counter[d] += 1
+
+    print(*counter.values(), sep='\n')      # * : 모든 bracket들 제거해주고 표시
+
+def q10872():
+    def factorial(n):       # failed 2 time
+        if n == 0:
+            return 1
+        
+        return n * factorial(n-1)
+
+    N = int(input())
+
+    print(factorial(N))
